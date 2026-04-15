bc70443b966ea68f71da5d8cafe24a11fdfc25a9
Doug Challener <doug@challener.net>
2026-04-14 19:29:28 -0500

    Initial Commit

---
+++ Antibiotic Guru/Antibiotic_GuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Antibiotic_GuruApp.swift
+//  Antibiotic Guru
+//
+//  Created by Doug Challener on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Antibiotic_GuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
